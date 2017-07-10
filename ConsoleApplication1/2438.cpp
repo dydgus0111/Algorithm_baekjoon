915a5bf4e179cf11a89271c06db47b207f88e7b9
yonghyeonKim <kkskww@gmail.com>
2017-07-10 21:50:42 +0900

    2438

---
+++ ConsoleApplication1/2438.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int num, i, j, k;
+	scanf("%d", &num);
+
+	for (i = 1; i <= num; i++) {
+		for (k = num - 1; k >= i; k--) {
+			printf(" ");
+		}
+			for (j = 0; j < i; j++) {
+				printf("*");
+			}
+		printf("\n");
+
+	}
+}
---
+++ ConsoleApplication1/2741.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int n=1,i;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		printf("%d\n", i);
+	return 0;
+}
