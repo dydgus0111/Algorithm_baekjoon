187776f6611d06b1cbfd27c6d5eaca77f4973da0
yonghyeonKim <kkskww@gmail.com>
2017-07-10 21:52:28 +0900

    2439 / for문사용 5번까지

--- ConsoleApplication1/2439.cpp
+++ ConsoleApplication1/2439.cpp
@@ -6,9 +6,12 @@ int main()
 	scanf("%d", &num);
 
 	for (i = 1; i <= num; i++) {
-			for (j = 0; j < i; j++) {
-				printf("*");
-			}
+		for (k = num - 1; k >= i; k--) {
+			printf(" ");
+		}
+		for (j = 0; j < i; j++) {
+			printf("*");
+		}
 		printf("\n");
 
 	}
